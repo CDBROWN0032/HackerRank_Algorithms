87715631a575af92aef3bcae04386fe945af894b
Christopher Brown <christopher.d.brown1@outlook.com>
2018-06-20 13:19:49 -0400

    time conversion

--- 10 Time Conversion.cs
+++ 10 Time Conversion.cs
@@ -13,9 +13,23 @@ class Solution {
         s = s.Replace("PM", ":PM").Replace("AM", ":AM");
         string convertedTime = "";
         string[] time = s.Split(':');
-        if(time[3] == "PM") {
+        if (time[0] == "12" && time[3] == "AM") {
+                time[0] = "00";
+        }
+        else if (time[0] == "12" && time[3] == "PM") {
+        }
+        else if(time[3] == "PM") {
             Int32.TryParse(time[0], out int x);
             x += 12;
+            if (x == 12 && time[3] == "AM") {
+                x = 00;
+            }
+            else if (x == 24) {
+                x = 00;
+            }
+            else if (x > 24) {
+                x -= 12;
+            }
             time[0] = x.ToString();
         }
         for(int i = 0; i < 3; i++) {
@@ -25,7 +39,6 @@ class Solution {
             }
             
         }
-         
         return convertedTime;
         }
 
